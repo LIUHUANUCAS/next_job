a533789d5a21e4d9623b28dde80613ff205fc5f5
LIUHUAN <liu2012huan@126.com>
2021-01-24 23:21:56 +0800

    [update] delete leaves with a given value using dfs post order traversal

---
+++ leetcode/tree/1325-Delete-Leaves-With-a-Given-Value.cpp
@@ -0,0 +1,41 @@
+#include <cstdio>
+#include <iostream>
+#include <random>
+#include <unordered_map>
+#include <vector>
+#include "../data_type.h"
+
+using namespace std;
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
+ * };
+ */
+class Solution {
+   public:
+    TreeNode* removeLeafNodes(TreeNode* root, int target) {
+        return dfs(root, target);
+    }
+    TreeNode* dfs(TreeNode* root, int val) {
+        if (root->left)
+            root->left = dfs(root->left, val);
+        if (root->right)
+            root->right = dfs(root->right, val);
+
+        if (root->val == val && root->left == nullptr && root->right == nullptr)
+            return nullptr;
+        return root;
+    }
+};
+int main() {
+    Solution so;
+
+    return 0;
+}
