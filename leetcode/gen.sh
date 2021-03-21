7c5b7a407767cef710588a8f83b87362260f7d23
LIUHUAN <liu2012huan@126.com>
2021-03-22 00:00:32 +0800

    [update] generate file name

--- leetcode/gen.sh
+++ leetcode/gen.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
+
+bname=`basename $0`
+if [ $# -ne 1 ];then 
+    echo "./$bname '1. Two Sum' => 1-Two-Sum.cpp"
+    exit 0
+fi
+
 curdir=`dirname $0`
-name=`bash $curdir/generate_filename.sh "$1"`
+name=`bash $curdir/get-filename.sh "$1"`
 echo $name
 src="$curdir/00-tamplete-file.cpp"
 cp  "$src" $name
