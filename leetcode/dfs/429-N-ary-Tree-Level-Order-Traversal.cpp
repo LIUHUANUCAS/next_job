a361c4f094c493262eaf849c7deec71549e75ddd
LIUHUAN <liu2012huan@126.com>
2021-04-20 23:09:59 +0800

    [update] bfs for level order traversal

--- leetcode/dfs/429-N-ary-Tree-Level-Order-Traversal.cpp
+++ leetcode/dfs/429-N-ary-Tree-Level-Order-Traversal.cpp
@@ -1,5 +1,6 @@
 #include <cstdio>
 #include <iostream>
+#include <queue>
 #include <random>
 #include <stack>
 #include <unordered_map>
@@ -12,11 +13,8 @@ class Node {
    public:
     int val;
     vector<Node*> children;
-
     Node() {}
-
     Node(int _val) { val = _val; }
-
     Node(int _val, vector<Node*> _children) {
         val = _val;
         children = _children;
@@ -28,7 +26,28 @@ class Solution {
     vector<vector<int>> res;
     vector<vector<int>> levelOrder(Node* root) {
         dfs(root, 0);
+        // bfs(root,res);
         return res;
+    }
+    void bfs(Node* root, vector<vector<int>>& r) {
+        if (root == nullptr)
+            return;
+        queue<Node*> que;
+        que.push(root);
+        while (!que.empty()) {
+            int size = que.size();
+            r.push_back({});
+            for (int i = 0; i < size; i++) {
+                auto x = que.front();
+                que.pop();
+                r.back().push_back(x->val);
+                for (auto e : x->children) {
+                    if (e != nullptr) {
+                        que.push(e);
+                    }
+                }
+            }
+        }
     }
     void dfs(Node* root, int level) {
         if (root == nullptr)
