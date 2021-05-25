6a05db44d2532b882e15dc5bbdaacbe26f26c20a
LIUHUAN <liu2012huan@126.com>
2021-05-25 08:48:36 +0800

    [update] diff ways to add parentheses

---
+++ leetcode/divide-conquer/241-Different-Ways-to-Add-Parentheses.cpp
@@ -0,0 +1,89 @@
+#include <cstdio>
+#include <iostream>
+#include <map>
+#include <random>
+#include <set>
+#include <stack>
+#include <unordered_map>
+#include <vector>
+
+#include "../data_type.h"
+
+using namespace std;
+class Solution {
+   public:
+    vector<int> diffWaysToCompute(string exp) {
+        if (exp.empty())
+            return {};
+        // cout << exp << endl;
+        return dfs(exp, 0, exp.size() - 1);
+    }
+
+    vector<int> dfs(string& exp, int lo, int hi) {
+        if (lo > hi)
+            return {};
+
+        vector<int> res;
+        for (int i = lo; i <= hi; i++) {
+            if (ismul(exp[i])) {
+                vector<int> L = dfs(exp, lo, i - 1);
+                vector<int> R = dfs(exp, i + 1, hi);
+                dup(res, L, R, exp[i]);
+            }
+        }
+        if (res.empty()) {
+            string t(exp.begin() + lo, exp.begin() + hi + 1);
+            int c = atoi(t.c_str());
+            res.push_back(c);
+        }
+
+        return res;
+    }
+    void dup(vector<int>& res, vector<int>& L, vector<int>& R, char ch) {
+        if (L.empty()) {
+            for (auto e : R) {
+                res.push_back(e);
+            }
+        }
+        if (R.empty()) {
+            for (auto e : L) {
+                res.push_back(e);
+            }
+        }
+        auto sp = ch;
+        for (auto e : L) {
+            for (auto r : R) {
+                int c = 0;
+                switch (sp) {
+                    case '+':
+                        c = e + r;
+                        break;
+                    case '-':
+                        c = e - r;
+                        break;
+                    case '*':
+                        c = e * r;
+                        break;
+                }
+                res.push_back(c);
+            }
+        }
+    }
+    int ismul(char ch) { return ch == '+' || ch == '-' || ch == '*'; }
+};
+int main() {
+    Solution so;
+    string expression;
+    vector<int> r;
+    {
+        expression = "2-1-1";
+        r = so.diffWaysToCompute(expression);
+        printvector(r);
+    }
+    {
+        expression = "2*3-4*5";
+        r = so.diffWaysToCompute(expression);
+        printvector(r);
+    }
+    return 0;
+}
