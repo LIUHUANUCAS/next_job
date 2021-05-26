8ee548840a2e077fc65c5a7e904acdede265334e
LIUHUAN <liu2012huan@126.com>
2021-05-26 12:02:11 +0800

    [update] gfs project

---
+++ project/gfs/gfs.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+)
+
+// MetaData in master
+type MetaData struct {
+}
+
+// Chunk data
+type Chunk struct {
+}
+
+// SnapShot for file
+type SnapShot struct {
+}
+
+func main() {
+	fmt.Println("GFS")
+}
