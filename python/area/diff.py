2b12b28a8c0832abb818878167936ea77f950681
LIUHUAN <liu2012huan@126.com>
2022-04-24 00:42:37 +0800

    [update] filename & format output

--- python/area/diff.py
+++ python/area/diff.py
@@ -10,7 +10,12 @@ sys.setdefaultencoding('utf8')
 
 ## table name for update 
 tableName='risk'
-SQLCOMMENT="-- "
+SQLCOMMENT="--"
+
+ADD_LABEL = 'add'
+UPDATE_LABEL='update'
+DEL_LABEL='del'
+NONE_LABEL='none'
 
 level_map = {
     '1': "中",
@@ -19,6 +24,15 @@ level_map = {
 }
 
 
+update_log_map = {
+    '0': "无需更新",
+    '1': "添加记录",
+    '2': "删除记录",
+    '3': "更新记录",
+}
+
+row_keys = ['qhdm' ,'risk', 'sfmc' , 'dsmc' , 'gxsj']
+
 def get_level_label(level):
     l = str(level)
     if l not in level_map.keys():
@@ -26,21 +40,15 @@ def get_level_label(level):
 
     return level_map[l]
 
-row_keys = ['qhdm' ,'risk', 'sfmc' , 'dsmc' , 'gxsj']
 def load(filename):
     data_dict = {}
     with open(filename) as f:
         for line in f.readlines():
             line = line.strip().split(',')
-            # print(line)
             prov = line[2].decode('utf-8')
             city = line[3].decode('utf-8')
-            print(SQLCOMMENT+'' + prov+','+city)
-            # row = [line[0],line[1],prov,city,line[4]]
-            row_dict = {
-                row_keys[i]:line[i] for i in range(len(row_keys)) 
-            }
-            # print(row_dict)
+            # print(SQLCOMMENT+'' + prov+','+city)
+            row_dict = {row_keys[i]:line[i] for i in range(len(row_keys)) }
             data_dict[line[0]] = row_dict
     return data_dict
 
@@ -56,16 +64,16 @@ def sql_comment_print(d):
     s = [str(e) for k,e in d.items()]
     print("%s %s",SQLCOMMENT,s)
 
-def print_with_sql_comment(s):
-    print("%s %s"%(SQLCOMMENT,s))
+def print_with_sql_comment(type,s):
+    print("%s %d %s"%(SQLCOMMENT,type,s))
 
 
 def getAction(newLevel, oldLevel):
     a, b = int(str(newLevel)),int(str(oldLevel))
     if a < b:
-        return "降低"
+        return "降"
     elif a > b:
-        return "升级"
+        return "升"
     return "保持"
 
 def generate_add_sql(a):
@@ -78,7 +86,7 @@ def generate_add_sql(a):
 
 def generate_del_sql(a):
     oldLevel = a['old_risk']
-    s = "delete from %s where qhdm = '%s'; %s %s-%s" %(tableName,a['qhdm'],SQLCOMMENT ,a['sfmc'],a['dsmc'])
+    s = "delete from %s where qhdm = '%s'; %s %s-%s-%s" %(tableName,a['qhdm'],SQLCOMMENT ,a['sfmc'],a['dsmc'],get_level_label(oldLevel))
     # print(s)
     new_risk_label = get_level_label(a['risk'])
     old_risk_label = get_level_label(oldLevel)
@@ -88,17 +96,17 @@ def generate_del_sql(a):
 
 def generate_update_sql(a):
     oldLevel = a['old_risk']
-    s = "update %s set risk = '%s' where qhdm = '%s'; %s %s-%s" %(tableName,a['risk'],a['qhdm'],SQLCOMMENT,a['sfmc'],a['dsmc'])
     # print(s)
     up = getAction(a['risk'],oldLevel)
     new_level_lable = get_level_label(a['risk'])
     old_level_lable = get_level_label(oldLevel)
+    s = "update %s set risk = '%s' where qhdm = '%s'; %s %s-%s-%s->%s" %(tableName,a['risk'],a['qhdm'],SQLCOMMENT,a['sfmc'],a['dsmc'],old_level_lable,new_level_lable)
 
     log = "%s%s由%s风险%s为%s风险"%(a['sfmc'],a['dsmc'],old_level_lable,up,new_level_lable)
     return s,log
 
 def generate_nosql(a):
-    s = [SQLCOMMENT] + [str(v) for k,v in a.items()]
+    s = [SQLCOMMENT,'nosql'] + [str(v) for k,v in a.items()]
     s = ','.join([str(e) for e in s])
     # print(s)
     old_level_label = get_level_label(a['old_risk'])
@@ -108,17 +116,17 @@ def generate_nosql(a):
     return s ,log 
 
 gen_map = {
-    'add':generate_add_sql,
-    "del": generate_del_sql,
-    "update":generate_update_sql,
-    "none":generate_nosql,
+    'add':[generate_add_sql,1],
+    "del": [generate_del_sql,2],
+    "update":[generate_update_sql,3],
+    "none":[generate_nosql,0],
 }
 
 
 def generate_sql(a,type):
-    
     if type in gen_map.keys():
-        return gen_map[type](a)
+        return gen_map[type][0](a)
+    
     return SQLCOMMENT,SQLCOMMENT
 
 def add_log(update_map,log,type):
@@ -126,32 +134,40 @@ def add_log(update_map,log,type):
         update_map[type] = []
     update_map[type].append(log)
 
+def add_result(result_map,result,type):
+    if type not in result_map.keys():
+        result_map[type] = []
+    result_map[type].append(result)
+
 def diff_data(old,new):
-    result = []
+    result = {}
     update_log = {}
     b_a = set_substract(new,old)
+
     #1. B - A , just add rows 
     for key in b_a: 
         # result.append(new[key] + ['add'])
         deepmap = copy.deepcopy(new[key])
-        deepmap['type'] = 'add'
+        deepmap['type'] = ADD_LABEL
         deepmap['old_risk'] = '0'
-        result.append(deepmap)
+        # result.append(deepmap)
+        add_result(result,deepmap,ADD_LABEL)
         _, log = generate_add_sql(deepmap)
-        add_log(update_log,log,'add')
+        add_log(update_log,log,ADD_LABEL)
 
     # 2. A - B , just delete     
     a_b = set_substract(old,new)
     for key in a_b:
         # result.append(old[key] + ['del'])
         deepmap = copy.deepcopy(old[key])
-        deepmap['type'] = 'del'
+        deepmap['type'] = DEL_LABEL
         deepmap['risk'] = '0'
         deepmap['old_risk'] = old[key]['risk']
-        result.append(deepmap)
+        # result.append(deepmap)
+        add_result(result,deepmap,DEL_LABEL)
         _,log = generate_del_sql(deepmap)
         # update_log.append(log)
-        add_log(update_log,log,'del')
+        add_log(update_log,log,DEL_LABEL)
     
     # 3. A and B 
     comm = intercept(new,old)
@@ -162,45 +178,67 @@ def diff_data(old,new):
             level = level_new
 
             deepmap = copy.deepcopy(old[key])
-            deepmap['type'] = 'update'
+            deepmap['type'] = UPDATE_LABEL
             deepmap['risk'] = level 
             deepmap['old_risk'] = level_old
-            result.append(deepmap)
+            add_result(result,deepmap,UPDATE_LABEL)
+            # result.append(deepmap)
             _,log = generate_update_sql(deepmap)
-            add_log(update_log,log,'update')
+            add_log(update_log,log,UPDATE_LABEL)
         else :
             deepmap = copy.deepcopy(old[key])
-            deepmap['type'] = 'none'
+            deepmap['type'] = NONE_LABEL
             deepmap['old_risk'] = level_old
-            result.append(deepmap)
+            add_result(result,deepmap,NONE_LABEL)
+            # result.append(deepmap)
             generate_nosql(deepmap)
+
     return result,update_log
 
 
-newfile=sys.argv[1]
-oldfile=sys.argv[2]
-new = load(newfile)
-old = load(oldfile)
-print("%s update %s"%(SQLCOMMENT,newfile))
-result,log  = diff_data(old,new)
-result.sort(key=lambda x: int(x['qhdm']))
-# print(result)
+def get_type_sql_log(array,type):
+    sql_list,log_list = [],[]
+    for item in  array:
+        res = generate_sql(item,type)
+        sql_list.append(res[0])
+        log_list.append(res[1])
+    return sql_list,log_list
+    
+if __name__ == '__main__':
+    if len(sys.argv) != 3 :
+        print('python %s %s %s'% (sys.argv[0],'new_file','old_file'))
+        sys.exit(-1)
+    newfile=sys.argv[1]
+    oldfile=sys.argv[2]
+    new = load(newfile)
+    old = load(oldfile)
+    print("%s update %s"%(SQLCOMMENT,newfile))
+    result_map,log  = diff_data(old,new)
+    # result.sort(key=lambda x: int(x['qhdm']))
+    # print(result)
+    logs = {}
+    for type,result in result_map.items() :
+        result.sort(key=lambda x : int(x['qhdm']))
+        sql_list,log_list = get_type_sql_log(result,type)
+        for sql in sql_list:
+            print(sql)
+        logs[type] = log_list 
 
-for e in result :
-    # print(e)
-    s = generate_sql(e,e['type'])
-    print(s[0])
-    print_with_sql_comment(s[1])
-    print('-----------------------')
+    # 修改日志
+    print('----------更新记录----------')
+    print('/***\n')
+    i=0
+    for type, log in logs.items():
+        i+=1
+        update_log_label = update_log_map[str(gen_map[type][1])]
+        print('%d.%s:'%(i,update_log_label))
+        for idx,item in enumerate(log):
+            print('\t%d) %s'%(idx+1,item))
+            # print_with_sql_comment(gen_map[type][1], item)
+    print('**/\n')
+    # "中高风险区域表已更新，更新内容：1）删除记录：河南省商丘市由中风险降为低风险 2）新增记录：吉林省白城市升为中风险"
 
-# for e in result:
-#     s = generate_sql(e,e['type'])
-#     print(s[1])
 
-
-# "中高风险区域表已更新，更新内容：1）删除记录：河南省商丘市由中风险降为低风险 2）新增记录：吉林省白城市升为中风险"
-
-
-# 1): 更新内容: prov-city-由$high/$mid风险降低为$low风险
-# 2): 新增记录: prov-city升为$high/$mid风险"
- 
+    # 1): 更新内容: prov-city-由$high/$mid风险降低为$low风险
+    # 2): 新增记录: prov-city升为$high/$mid风险"
+    
---
+++ python/area/generate-risk_loc-file.py
@@ -0,0 +1,97 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+import datetime
+import json 
+import sys
+
+reload(sys)
+
+sys.setdefaultencoding('utf8')
+
+today = '2022-04-23'
+today = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+whitelist = ['北京市','上海市','重庆市','天津市']
+row_keys = ['qhdm' ,'risk', 'sfmc' , 'dsmc' , 'gxsj']
+cityname='city-code.txt'
+data_source_url='https://bmfw.www.gov.cn/yqfxdjcx/risk.html'
+
+def load_json(filename):
+    with open(filename) as f :
+        data = json.load(f)
+        return data
+
+##{cityname:citycode} example:{'上海市':'310000'}
+def load_city_name_map(filename): 
+    with open(filename) as f :
+        city_map ={}
+        for line in f.readlines():
+            line = line.strip()
+            fields = line.split(' ')
+            name = fields[1].decode('utf-8')
+            city_map[name] = fields[0]
+        return city_map
+
+def generate_format_data(data,city_code,flag):
+    res = {}
+    
+    # [qhdm ,risk, sfmc , dsmc , gxsj]
+    for e in data:
+        prov = e['province']
+        city = e['city']
+        query_city = city 
+        if prov in whitelist:
+            city = ''
+            query_city = prov  
+        if city_code[query_city] is not None: 
+            code = city_code[query_city]
+            prov_decode = prov
+            line =[code,str(flag),prov_decode,city,today]
+            row_dict = {row_keys[i]:line[i] for i in range(len(row_keys)) }
+            # print(row_dict)
+            res[code] = row_dict
+            # res[code]= [code,flag,prov_decode,city,today] 
+    # print(res)
+    return res
+
+def merge_middle_high(mid,high):
+    res = mid 
+    for k,v in high.items():
+        if k in mid.keys():
+            mid[k]['risk'] = max(mid[k]['risk'],v['risk'])
+        else:
+            res[k] = v
+    return res 
+
+def get_list(data_dict):
+    res = [e for _, e  in data_dict.items()]
+    res.sort(key=lambda x:int(x['qhdm']))
+    return res 
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print('data source url:',data_source_url)
+        print('usage: python %s %s'%(sys.argv[0],'loc_risk_filename'))
+        sys.exit(-1)
+    filename='0423.txt'
+    filename=  sys.argv[1]
+    data = load_json(filename) 
+    high = data['data']['highlist']
+    mid = data['data']['middlelist']
+    end_update_time = data['data']['end_update_time']
+    print(end_update_time)
+    city_code_map = load_city_name_map(cityname)
+    # print(city_code_map)
+    high_map = generate_format_data(high,city_code_map,2)
+    mid_map = generate_format_data(mid,city_code_map,1)
+
+    result_loc_map = merge_middle_high(mid_map,high_map)
+
+    loc_list = get_list(result_loc_map)
+    output_filename=datetime.datetime.now().strftime('%Y-%m-%d')
+    with open(output_filename+'_new_risk_loc.txt','w') as f:
+        for e in loc_list:
+            row = [e[row_keys[i]] for i in range(len(row_keys))]
+            
+            s = ','.join(row)
+            f.write(s+'\n')
